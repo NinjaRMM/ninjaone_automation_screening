e7614b251deb1900c4db815b8212292946644754
maurolgonzalez <maurolgonzalez@gmail.com>
2022-09-25 15:48:16 -0300

    Complete step4

--- step4/PowershellChallenge.ps1
+++ step4/PowershellChallenge.ps1
@@ -3,8 +3,18 @@
 #Please review the below code and answer the following questions to the best of your ability: 
 
 #1.) What is this code doing?
-#2.) What does the program assume you need to have access to in order to execute? 
+<#This script first run the commands to check if a key exist in the Reg Key on a VM called WPM. This key is necessary to install NinjaRMM.
+Then check if the installer is on the same VM.#>
+#2.) What does the program assume you need to have access to in order to execute?
+<#
+* Read access to the folder containing the VM.
+* Virtual Machine.Interaction.Console Interaction privilege.
+* The virtual machines must be powered on and have VMware Tools installed.
+* Network connectivity to the ESX system hosting the virtual machine on port 902.
+* The guest account you use to authenticate with the guest operating system must have administrator's privileges
+#>
 #3.) What architectures can this script run on? 
+# Due the reg key path use WOW6432Node it only runs on a 64 bits architecture.
 
 $vm = Get-VM -Name WPM
 $IsNinjaInstalled = "Test-Path 'HKLM:\SOFTWARE\WOW6432Node\NinjaRMM LLC\NinjaWPM'"
