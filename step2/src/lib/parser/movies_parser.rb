e9eb08990aab5ea6d4b0a951748e86565893a51c
Luã Lázaro <lualjsantos@gmail.com>
2022-10-10 16:40:55 -0300

    [UPDATE] Add documentation to the MoviesParser class

--- step2/src/lib/parser/movies_parser.rb
+++ step2/src/lib/parser/movies_parser.rb
@@ -1,11 +1,18 @@
 
 class MoviesParser
+  # Selects the movies according to a target decade
+  # @param movies [Array<Hash>] The source movies list
+  # @param target_decade [Integer] The target decade
+  # @return [Array<Hash>] The movies from the target decade
   def self.movies_from_decade( movies: , target_decade:)
     movies.select{|movie| decade(movie['year'].to_i) == target_decade }
   end
 
   private
 
+  # Obtains the decade from a given year
+  # @param year [Integer] The source year
+  # @return [Integer] The decade of the given year
   def self.decade(year)
     (year/10 * 10).to_s[-2..-1].to_i
   end
