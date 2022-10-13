679bba7468d0eeac8db0c2d4fbc29efded22d56c
Luã Lázaro <lualjsantos@gmail.com>
2022-10-12 23:07:47 -0300

    [UPDATE] Add comments on the step1_enabled_iii.py script

--- step1/src/step1_enabled_iii.py
+++ step1/src/step1_enabled_iii.py
@@ -1,5 +1,18 @@
 import subprocess
 
+# Comments:
+# On my research I first went through 2 directions:
+# 1. An API to interact through terminal.
+# 2. Getting this information from the Windows Registry
+#
+# For the nunmber 1, I couldn´t find something easy to implement,
+# while for number 2, I found results very dependend on the OS version.
+# I realized that the policy to disable automatic updates are different
+# from one Win version to another, with distict locations on the Registry.
+#
+# Due to that, I looked foward getting the status of the Windows Update service,
+# because if it is not running, the automatic updates will not happen.
+
 def isWindowsUpdateEnable():
     print('Checkinig if the Windows Update is running:')
     result = subprocess.check_output(['sc', 'query', 'wuauserv'])
