873b7098d6781988036180f4f711bc13ddc3e162
Guilherme Braga <gbvbahia01@gmail.com>
2023-01-07 17:25:27 -0300

    Update PowershellChallenge.ps1

--- step4/PowershellChallenge.ps1
+++ step4/PowershellChallenge.ps1
@@ -2,9 +2,27 @@
  
 #Please review the below code and answer the following questions to the best of your ability: 
 
+### *** My knowledge on shell script is very basic, I try my best. ***
+
 #1.) What is this code doing?
+After search Get-VM looks a command from Azure, you are geting a VM named WPM
+Declare a variable $IsNinjaInstalled
+Runs the command $IsNinjaInstalled on the VM called WPM with user and password
+Capture the command result on $Result
+Check if the $Result was not ok, if not stop the process.
+Remove spaces before and after and set the result at $output
+Verify if the $output value is true
+Not sure what is Write-Host, looks a log
+New variable $isNinjaWPMDownloaded declared
+Runs the command $isNinjaWPMDownloaded on the VM called WPM with user and password
+Check if the $Result was not ok, if not stop the process with error exit.
+End the process with success.
+
 #2.) What does the program assume you need to have access to in order to execute? 
+You need to have access to the Get-VM and execute commands there.
+
 #3.) What architectures can this script run on? 
+Wndows
 
 $vm = Get-VM -Name WPM
 $IsNinjaInstalled = "Test-Path 'HKLM:\SOFTWARE\WOW6432Node\NinjaRMM LLC\NinjaWPM'"
