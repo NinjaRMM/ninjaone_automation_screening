3eec8d614cb9a8d21cd9b57d1eed6fdae6826a2d
Bernardo <oliveira.bernardocb@gmail.com>
2023-01-12 23:14:51 -0300

    Add boilerplate code from Spring Initializr

---
+++ step2/step2/src/main/java/com/ninjaone/step2/Step2Application.java
@@ -0,0 +1,13 @@
+package com.ninjaone.step2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Step2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Step2Application.class, args);
+	}
+
+}
---
+++ step2/step2/src/test/java/com/ninjaone/step2/Step2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ninjaone.step2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Step2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
