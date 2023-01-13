97963af5624b07d97e150a7489fc9c5e618986c2
Bernardo <oliveira.bernardocb@gmail.com>
2023-01-12 23:25:34 -0300

    Add step3 boilerplate code from Spring Initializr

---
+++ step3/src/main/java/com/ninjaone/step3/Step3Application.java
@@ -0,0 +1,13 @@
+package com.ninjaone.step3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Step3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Step3Application.class, args);
+	}
+
+}
