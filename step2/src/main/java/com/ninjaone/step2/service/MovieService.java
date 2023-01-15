114bd6bc1322978bd1789e416987ea25e89e1a09
Bernardo <oliveira.bernardocb@gmail.com>
2023-01-15 20:01:51 -0300

    Switch tabs for spaces (Step2)

--- step2/src/main/java/com/ninjaone/step2/service/MovieService.java
+++ step2/src/main/java/com/ninjaone/step2/service/MovieService.java
@@ -11,10 +11,10 @@ import com.ninjaone.step2.model.Movie;
 
 public interface MovieService {
 
-	public Collection<Movie> extractMoviesFromFile(File pathName) throws IOException;
+    public Collection<Movie> extractMoviesFromFile(File pathName) throws IOException;
 
-	public Collection<Movie> filterMoviesByTimePeriod(List<Movie> allMovies, Integer startYear, Integer endYear);
+    public Collection<Movie> filterMoviesByTimePeriod(List<Movie> allMovies, Integer startYear, Integer endYear);
 
-	public void writeMoviesToFile(Collection<Movie> movies, File pathName) throws StreamWriteException, DatabindException, IOException;
+    public void writeMoviesToFile(Collection<Movie> movies, File pathName) throws StreamWriteException, DatabindException, IOException;
 
 }
