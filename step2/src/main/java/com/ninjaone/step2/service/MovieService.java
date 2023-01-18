482612fb8504e605421e85af080b36bc164e2c7c
Bernardo <oliveira.bernardocb@gmail.com>
2023-01-18 01:31:56 -0300

    Add 'validateDecade' to MovieService

--- step2/src/main/java/com/ninjaone/step2/service/MovieService.java
+++ step2/src/main/java/com/ninjaone/step2/service/MovieService.java
@@ -17,4 +17,16 @@ public interface MovieService {
 
     public void writeMoviesToFile(Collection<Movie> movies, File pathName) throws StreamWriteException, DatabindException, IOException;
 
+    public default void validateDecade(Integer decade) {
+        if (decade == null) {
+            throw new IllegalArgumentException("Please provide the 'decade' parameter.");
+        } else if (decade % 10 != 0) {
+            throw new IllegalArgumentException("The parameter 'decade' must end in '0' - e.g. '80', '1910'.");
+        } else if ((decade < 1900 || decade > 2010) && (decade < 0 || decade > 90)) {
+            throw new IllegalArgumentException("The decade must be between '0' and '90' or '1900' and '2010'.");            
+        } else if (decade == 0 || decade == 10) {
+            throw new IllegalArgumentException("Ambiguous decade, as it could be either " + (1900 + decade) + " or " + (2000 + decade) + ". Please insert full year.");
+        }
+    }
+
 }
