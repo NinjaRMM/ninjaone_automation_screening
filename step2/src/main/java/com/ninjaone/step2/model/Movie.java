65e8f191722ba994e70fa5c12fa3545542f0c842
Bernardo <oliveira.bernardocb@gmail.com>
2023-01-18 01:15:07 -0300

    Fix Movie serialization problem

--- step2/src/main/java/com/ninjaone/step2/model/Movie.java
+++ step2/src/main/java/com/ninjaone/step2/model/Movie.java
@@ -2,13 +2,17 @@ package com.ninjaone.step2.model;
 
 import java.util.Set;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Movie {
 
     private String title;
