c33f8f5f86f38edec50012cbdab816c63d248d19
Bernardo <bernardo.oliveira@paymentology.com>
2023-01-22 09:16:09 -0300

    Create Javadocs for MovieService (Step2 & Step3)

--- step3/src/main/java/com/ninjaone/step3/service/MovieService.java
+++ step3/src/main/java/com/ninjaone/step3/service/MovieService.java
@@ -9,14 +9,36 @@ import com.fasterxml.jackson.core.exc.StreamWriteException;
 import com.fasterxml.jackson.databind.DatabindException;
 import com.ninjaone.step3.model.Movie;
 
+/**
+ * Service for filtering and de/serializing {@link Movie} collections.
+ */
 public interface MovieService {
 
+    /**
+     * Extracts objects from a JSON file and converts them into a {@link Collection} of {@link Movie}(s).
+     */
     public Collection<Movie> extractMoviesFromFile(File pathName) throws IOException;
 
+    /**
+     * From <b>allMovies</b>, returns only the {@link Movie}(s) which year is within the interval of <b>startYear</b>
+     * and <b>endYear</b> (inclusive for both).
+     */
     public Collection<Movie> filterMoviesByTimePeriod(List<Movie> allMovies, Integer startYear, Integer endYear);
 
+    /**
+     * Converts a {@link Collection} of {@link Movie}(s) into a JSON file in the directory specified by <b>pathName</b>.
+     */
     public void writeMoviesToFile(Collection<Movie> movies, File pathName) throws StreamWriteException, DatabindException, IOException;
 
+    /**
+     * Checks whether <b>decade</b> ends in "0" and is within the period from 1900 and 2010, which are the decades
+     * comprised by "movies.json". It also accepts values like 30, 80, as they represent 1930, 1980, respectively.
+     * <br>
+     * It doesn't accept "0" or "10", as they could be decades either from the 1900s and from the 2000s, so for such
+     * decades, please write the full-year decade - e.g. 1910, 2000.
+     * <br>
+     * Throws {@link IllegalArgumentException} should <b>decade</b> be non-compliant.
+     */
     public default void validateDecade(Integer decade) {
         if (decade == null) {
             throw new IllegalArgumentException("Please provide the 'decade' parameter.");
