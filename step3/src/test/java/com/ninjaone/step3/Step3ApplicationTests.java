69c463087ac34d930ce87946ae3c62d9b1e3d039
Bernardo <bernardo.oliveira@paymentology.com>
2023-01-25 10:58:35 -0300

    Revert "Add Cucumber boilerplate config for step3"
    This reverts commit 36a49e24340e2742150ac626570be8420a617bc8.

---
+++ step3/src/test/java/com/ninjaone/step3/Step3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ninjaone.step3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Step3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
