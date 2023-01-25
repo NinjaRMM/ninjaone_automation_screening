36a49e24340e2742150ac626570be8420a617bc8
Bernardo <oliveira.bernardocb@gmail.com>
2023-01-25 10:29:27 -0300

    Add Cucumber boilerplate config for step3

---
+++ step3/src/test/java/com/ninjaone/step3/configuration/CucumberSpringConfig.java
@@ -0,0 +1,10 @@
+package com.ninjaone.step3.configuration;
+
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@CucumberContextConfiguration
+@SpringBootTest
+public class CucumberSpringConfig {
+
+}
