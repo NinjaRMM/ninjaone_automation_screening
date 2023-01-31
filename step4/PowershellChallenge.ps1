31c35f821573ba5282ce192f66c7cf24cb53b27e
Edvan Soares <edvan.soares@zebra.com>
2023-01-31 00:33:59 -0300

    [Step 4] Answering questions

--- step4/PowershellChallenge.ps1
+++ step4/PowershellChallenge.ps1
@@ -3,8 +3,23 @@
 #Please review the below code and answer the following questions to the best of your ability: 
 
 #1.) What is this code doing?
+
+# Check if path on Windows registry key for NinjaRMM and NinjaWPM are installed. To run those scripts on VM user Ninja and password helloword is used. If there is any error on
+# script execution an error message will be showed and the execution will be terminated with code 1 (error). If there is no error the result will be checked and in case of true a message
+# informing that NinjaWPM was found to be installed will be printed.
+#
+# After that the following command check if NinjaWPM.exe is downloaded in c:\NinjaInstaller folder. If there is any error the execution will be terminated with error
+# indication, otherwise it will be finished successfully (code 0).
+
 #2.) What does the program assume you need to have access to in order to execute? 
+
+# The correct credentials and necessary permissions to run the script.
+
+
 #3.) What architectures can this script run on? 
+
+# x86-64 (Windows). The HKLM:\SOFTWARE\WOW6432Node key refers to 64-bits Windows version. It's used to present a separate view
+# for 32-bits applications that run on a 64-bits version.
 
 $vm = Get-VM -Name WPM
 $IsNinjaInstalled = "Test-Path 'HKLM:\SOFTWARE\WOW6432Node\NinjaRMM LLC\NinjaWPM'"
